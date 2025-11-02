11f7e265248844454a1a914230abd6234dfcc9b9
Melotwo <turoka15@gmail.com>
2025-11-02 20:31:02 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
-/**
- * @type {import('postcss-load-config').Config}
- *
- * NOTE: This file uses the .cjs extension to force CommonJS loading,
- * which resolves the "plugins.forEach is not a function" error when
- * package.json is set to "type": "module".
- */
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
