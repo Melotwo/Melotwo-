a6cb6c3632bdf8141de73aef402cea6f5052fa1b
Melotwo <turoka15@gmail.com>
2025-11-02 20:33:24 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+/**
+ * @type {import('postcss-load-config').Config}
+ *
+ * This uses the .cjs extension and CommonJS syntax (module.exports / require)
+ * to resolve module conflicts caused by "type": "module" in package.json.
+ */
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
