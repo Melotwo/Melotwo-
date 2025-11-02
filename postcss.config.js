fefaf5c84476203ba3c4bb646069261508066fbc
Melotwo <turoka15@gmail.com>
2025-11-02 20:25:28 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
+// This uses ES Module syntax (import/export) to comply with "type": "module" in package.json.
+
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 /** @type {import('postcss-load-config').Config} */
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+export default {
+  // PostCSS typically expects plugins to be passed as an array of functions.
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
 }
